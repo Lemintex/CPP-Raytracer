aa08ef1cdd28c0a7268b3a1016cfdda9b4fec2c2
Lemintex <aaron_nobile@hotmail.co.uk>
2023-09-06 22:49:32 +0100

    Added debug and tidied code

--- src/main.cpp
+++ src/main.cpp
@@ -1,32 +1,31 @@
 #include <iostream>
 
-// created left-.right, top->bottom
+// created left->right, top->bottom
 int main()
 {
-
-    // Image
-
+    // dimensions
     const int image_width = 2160;
     const int image_height = 1440;
 
-    // Render
-
+    // render image
     std::cout << "P3\n"
               << image_width << ' ' << image_height << "\n255\n";
 
-    for (int j = image_height - 1; j >= 0; --j)
+    for (int i = image_height - 1; i >= 0; i--)
     {
-        for (int i = 0; i < image_width; ++i)
+        std::cerr << "Rendering scanline " << image_height - i << " of " << image_height << '\n';
+        for (int j = 0; j < image_width; j++)
         {
-            auto r = double(i) / (image_width - 1);
-            auto g = double(j) / (image_height - 1);
-            auto b = 0.25;
+            double r = double(i) / (image_width - 1);
+            double g = double(j) / (image_height - 1);
+            float b = 0.25;
 
-            int ir = static_cast<int>(255.999 * r);
-            int ig = static_cast<int>(255.999 * g);
-            int ib = static_cast<int>(255.999 * b);
+            int ir = 255 * r;
+            int ig = 255 * g;
+            int ib = 255 * b;
 
             std::cout << ir << ' ' << ig << ' ' << ib << '\n';
         }
     }
+    std::cerr << "Done!\n";
 }
