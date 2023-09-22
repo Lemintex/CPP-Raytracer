536b1b936d742413d3fad0c947306c671d374b06
Lemintex <aaron_nobile@hotmail.co.uk>
2023-09-22 19:17:52 +0100

    adding material to sphere

--- src/sphere.h
+++ src/sphere.h
@@ -10,10 +10,12 @@ class sphere : public surface
 public:
     point3d center;
     float radius;
+    material *mat_ptr;
 
 public:
     sphere() {}
-    sphere(point3d cen, float r) : center(cen), radius(r){};
+    sphere(point3d cen, float r) : center(cen), radius(r){}
+    sphere(point3d cen, float r, material *m) : center(cen), radius(r), mat_ptr(m) {}
 
     virtual bool hit(
         ray &r, interval ray_t, hit_record &rec) const override;
