abf5f5262e2c8fefe672ae0f661ef4ff1702c535
Lemintex <aaron_nobile@hotmail.co.uk>
2023-10-09 20:54:25 +0100

    Fixed inverse sphere collision

--- src/surface.h
+++ src/surface.h
@@ -12,13 +12,6 @@ struct hit_record
     vec3d p;
     vec3d normal;
     material *mat_ptr;
-    bool front_face;
-
-    inline void set_face_normal(const ray &r, const vec3d &outward_normal)
-    {
-        front_face = vec3d::dot(r.direction(), outward_normal) < 0;
-        normal = front_face ? outward_normal : -outward_normal;
-    }
 };
 
 class surface
